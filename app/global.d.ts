c4698ffd4487ce896708b83f8187d54a6f132ff0
Zequez <zequez@gmail.com>
2024-03-12 18:20:32 -0300

    Added a changing favicon

--- app/global.d.ts
+++
@@ -1,9 +0,0 @@
-import { JSXInternal as JSXI } from 'preact/src/jsx';
-
-declare module 'preact/src/jsx' {
-  namespace JSXInternal {
-    // interface HTMLAttributes<T> extends JSXI.HTMLAttributes<T> {
-    //   class?: any;
-    // }
-  }
-}
