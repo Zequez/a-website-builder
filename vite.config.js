841f299e25a9d9cf92e8b2bfade4d52bc6811f80
Zequez <zequez@gmail.com>
2024-03-15 00:59:28 -0300

    Crossing my fingers, this might be it

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import UnoCSS from 'unocss/vite';
-import elmPlugin from 'vite-plugin-elm';
 import preact from '@preact/preset-vite';
 import Icons from 'unplugin-icons/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  plugins: [preact(), elmPlugin(), UnoCSS(), Icons({ compiler: 'jsx' }), tsconfigPaths()],
-  root: resolve(__dirname, 'app'),
-  base: './',
-  build: {
-    outDir: resolve(__dirname, 'dist', 'app'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'app/index.html'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [preact(), UnoCSS(), Icons({ compiler: 'jsx' }), tsconfigPaths()],
+    root: resolve(__dirname, 'app'),
+    base: mode === 'production' ? '/app/' : '/',
+    build: {
+      outDir: resolve(__dirname, 'dist', 'app'),
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'app/index.html'),
+        },
       },
     },
-  },
+  };
 });
