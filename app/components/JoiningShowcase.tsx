4f6629d12590e02949a2ec80ba41b6fa81141bc8
Zequez <zequez@gmail.com>
2024-04-01 21:58:19 -0300

    JoiningShowcase tweaked animation

--- app/components/JoiningShowcase.tsx
+++ app/components/JoiningShowcase.tsx
@@ -1,28 +1,35 @@
-import { cx } from '@app/lib/utils';
+import { cx, useIsVisible } from '@app/lib/utils';
+import { useRef } from 'preact/hooks';
 import CheckCircle from '~icons/fa6-solid/circle-check';
 
 export default function JoiningShowcase() {
+  const divRef = useRef<HTMLDivElement>(null);
+  const isVisible = useIsVisible(divRef);
+  const columnStyle = (delay: number) =>
+    isVisible ? { animation: `fade-in 0.5s ease-in ${0.1 * delay}s forwards` } : {};
   return (
     <div class="pb-16 bg-slate-200">
       <div class="max-w-screen-lg mx-auto pt-8">
         <div
+          ref={divRef}
           class="text-center text-[70px] font-bold mb-8 text-slate-400 tracking-wider"
           style={{
             textShadow:
               '0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #fff, 0 0 50px #fff, 0 0 60px #fff, 0 0 70px #fff',
+            animation: isVisible ? 'fade-scale-in .75s ease-in forwards' : null,
           }}
         >
           JOIN
         </div>
         <div class="flex justify-evenly text-center">
-          <ValuePropositionColumn name="Visitor" icon="👤">
+          <ValuePropositionColumn name="Visitor" icon="👤" class="delay-0" style={columnStyle(0)}>
             <p class="mb-4 text-center  opacity-75">Use all available tools without an account</p>
             <ul class="list-disc-inside">
               <ValuePropItem>Create offline websites</ValuePropItem>
               <ValuePropItem>Export build</ValuePropItem>
             </ul>
           </ValuePropositionColumn>
-          <ValuePropositionColumn name="Guest" icon="🧍">
+          <ValuePropositionColumn name="Guest" icon="🧍" style={columnStyle(1)}>
             <p class="mb-4 text-center  opacity-75">Sign up for an account</p>
             <ul class="list-disc-inside">
               <ValuePropItem>Limited sites and files on server</ValuePropItem>
@@ -31,7 +38,12 @@ export default function JoiningShowcase() {
               <ValuePropItem>Export build web</ValuePropItem>
             </ul>
           </ValuePropositionColumn>
-          <ValuePropositionColumn name="Power Team" icon="🌼">
+          <ValuePropositionColumn
+            name="Power Team"
+            icon="🌼"
+            class="delay-1000"
+            style={columnStyle(2)}
+          >
             <p class="mb-4 text-center  opacity-75">
               Sign up for an account and pledge to pay-as-much-as-you-can monthly or yearly
               contribution
@@ -54,7 +66,13 @@ export default function JoiningShowcase() {
               </ValuePropItem>
             </ul>
           </ValuePropositionColumn>
-          <ValuePropositionColumn name="Power Team" power icon="⚡️">
+          <ValuePropositionColumn
+            name="Power Team"
+            power
+            icon="⚡️"
+            class="delay-1500"
+            style={columnStyle(3)}
+          >
             <p class="mb-4 text-center opacity-75">
               Sign up for an account and pledge more than $50 per month
             </p>
@@ -88,14 +106,22 @@ const ValuePropositionColumn = ({
   name,
   children,
   power = false,
+  class: _class,
+  style,
 }: {
   icon: string;
   name: string;
   children: any;
   power?: boolean;
+  class?: string;
+  style?: any;
 }) => {
+  console.log('STYLE', style);
   return (
-    <div class="w-1/4 border-r-[1px] border-black/10 last:border-none px-4">
+    <div
+      class={cx('w-1/4 border-r-[1px] border-black/10 last:border-none px-4 opacity-0', _class)}
+      style={style}
+    >
       <div class="text-[60px] -mb-2">{icon}</div>
       <div
         class={cx('text-xl text-slate-500 tracking-wider mb-2', {
